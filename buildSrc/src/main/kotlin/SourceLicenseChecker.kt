95eb3e1029722f18e8ec80fec0aafb7c862c1518
Da Risk <da_risk@geekorum.com>
2026-05-19 09:01:24 -0400

    build: update SourceLicenseChecker for android projects

--- buildSrc/src/main/kotlin/SourceLicenseChecker.kt
+++ buildSrc/src/main/kotlin/SourceLicenseChecker.kt
@@ -20,19 +20,29 @@
  */
 package com.geekorum.build
 
+import com.android.build.api.dsl.AndroidSourceSet
+import com.android.build.api.dsl.CommonExtension
+import com.android.build.gradle.DynamicFeaturePlugin
+import com.android.build.gradle.TestPlugin
 import com.hierynomus.gradle.license.LicenseBasePlugin
 import com.hierynomus.gradle.license.tasks.LicenseCheck
 import com.hierynomus.gradle.license.tasks.LicenseFormat
 import nl.javadude.gradle.plugins.license.License
 import nl.javadude.gradle.plugins.license.LicenseExtension
 import nl.javadude.gradle.plugins.license.LicensePlugin
+import org.gradle.api.NamedDomainObjectContainer
 import org.gradle.api.Project
 import org.gradle.api.Task
+import org.gradle.api.file.FileTree
+import org.gradle.api.tasks.TaskProvider
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.fileTree
 import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.named
+import org.gradle.kotlin.dsl.the
 import org.gradle.kotlin.dsl.withType
+import java.util.Locale
 
 internal fun Project.configureSourceLicenseChecker(): Unit {
     apply<LicensePlugin>()
@@ -42,6 +52,18 @@ internal fun Project.configureSourceLicenseChecker(): Unit {
         mapping("java", "SLASHSTAR_STYLE")
         mapping("kt", "SLASHSTAR_STYLE")
         excludes(listOf("**/*.webp", "**/*.png"))
+    }
+
+    // the LicensePlugin doesn't configure itself properly on DynamicFeaturePlugin
+    // Copied the code to configure it
+    plugins.withType<DynamicFeaturePlugin> {
+        configureAndroid()
+    }
+
+    // the LicensePlugin doesn't configure itself properly on Android Test plugin
+    // Copied the code to configure it
+    plugins.withType<TestPlugin> {
+        configureAndroid()
     }
 
     tasks {
@@ -72,3 +94,59 @@ internal fun Project.configureSourceLicenseChecker(): Unit {
     }
 
 }
+
+
+private fun Project.configureAndroid() {
+    val android = the<CommonExtension>()
+    configureSourceSetRule(android.sourceSets, "Android") { ss ->
+        @Suppress("DEPRECATION")
+        when (ss) {
+            // the dsl.AndroidSourceSet don't expose any getter, so we still need to cast it
+            is com.android.build.gradle.api.AndroidSourceSet -> {
+                val kotlinFileTrees  = ss.kotlin.directories.foldIndexed(fileTree() as FileTree) { index, acc, n ->
+                    if (index == 0) { // skip first as it's an empty tree created for typing
+                        fileTree(n)
+                    } else {
+                        acc + fileTree(n)
+                    }
+                }
+                ss.java.getSourceFiles() + ss.res.getSourceFiles() + fileTree(ss.manifest.srcFile) + kotlinFileTrees
+            }
+            else -> fileTree()
+        }
+    }
+}
+
+/**
+ * Dynamically create a task for each sourceSet, and register with check
+ */
+@Suppress("DefaultLocale")
+private fun Project.configureSourceSetRule(androidSourceSetContainer: NamedDomainObjectContainer<out AndroidSourceSet>,
+                                           taskInfix: String, sourceSetSources: (AndroidSourceSet) -> FileTree) {
+    // This follows the other check task pattern
+    androidSourceSetContainer.configureEach {
+        val sourceSetTaskName = "${LicenseBasePlugin.getLICENSE_TASK_BASE_NAME()}${taskInfix}${name.capitalize()}"
+        logger.info("Adding $sourceSetTaskName task for sourceSet $name")
+
+        val checkTask = tasks.register(sourceSetTaskName, LicenseCheck::class.java)
+        configureForSourceSet(this, checkTask, sourceSetSources)
+
+        // Add independent license task, which will perform format
+        val sourceSetFormatTaskName = "${LicenseBasePlugin.getFORMAT_TASK_BASE_NAME()}${taskInfix}${name.capitalize()}"
+        val formatTask = tasks.register(sourceSetFormatTaskName, LicenseFormat::class.java)
+        configureForSourceSet(this, formatTask, sourceSetSources)
+    }
+}
+
+private fun configureForSourceSet(sourceSet: AndroidSourceSet, task: TaskProvider<out License>, sourceSetSources: (AndroidSourceSet) -> FileTree) {
+    task.configure {
+        // Explicitly set description
+        description = "Scanning license on ${sourceSet.name} files"
+
+        // Default to all source files from SourceSet
+        source = sourceSetSources(sourceSet)
+    }
+}
+
+private fun String.capitalize() =
+    replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
